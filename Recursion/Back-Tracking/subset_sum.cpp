2f1f28248204d017770c338968ca031886bbb209
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-16 21:24:55 +0530

    Corrected Subset_SUm

--- Recursion/Back-Tracking/subset_sum.cpp
+++ Recursion/Back-Tracking/subset_sum.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-typedef vector <int> vi;
+//typedef vector <int> vi;
 
 int k=10;
 
@@ -11,34 +11,29 @@ bool check(int sum){
         return false;
 }
 
-void print(vi subset){
-        for(int i=0; i<subset.size(); i++) cout<<subset[i]<<" ";
+void print(int subset[], int sub_size){
+        for(int i=0; i<sub_size; i++) cout<<subset[i]<<" ";
         cout<<endl;
 }
 
-void subsetGen(int array[],int index, int size, int sum, vi subset, int t){
-        if(index==size) return;
-
-        if(sum==k) {
-                print(subset);
-                subsetGen(array, index,size,sum-array[index], subset, t-1);
+void subsetGen(int array[],int index, int size, int sum, int subset[], int t, int sub_size){
+        if(index==size) {
+                if(sum==k) print(subset,sub_size);
                 return;
         }
-        subset.resize(t+1);
-        for(int i=index; i<size; i++) {
-                if(check(sum+array[i])) {
-                        subset[t]=array[i];
-                        subsetGen(array,i+1,size,sum+array[i], subset,t+1);
-                }
+
+        subsetGen(array,index+1,size,sum,subset,t,sub_size);
+        if(check(sum+array[index]))  {
+                subset[t]=array[index];
+                subsetGen(array,index+1,size,sum+array[index],subset,t+1,sub_size+1);
         }
 }
 
 
 int main(){
-        int a[100],n;
+        int a[100],n, subset[100];
         cin>>n;
         for(int i=0; i<n; i++) cin>>a[i];
-        vi subset;
-        subsetGen(a,0,n,0, subset,0);
+        subsetGen(a,0,n,0, subset,0,0);
         return 0;
 }
