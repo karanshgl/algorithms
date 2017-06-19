930bc9b7b2f0309cc534356fc97fb05afa7d03ec
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-20 00:45:18 +0530

    Added matrix rotation + minor changes

---
+++ Basic/rotatematrix_anticlockwise.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+void rotate(int arr[][1000], int m){
+        int rot[m][m];
+        int r=0,c=0;
+        for(int i=m-1; i>=0; i--) {
+                for(int j=0; j<m; j++) {
+                        rot[r][c++]=arr[j][i];
+                }
+                r++;
+        }
+
+        for(int i=0; i<m; i++) {
+                for(int j=0; j<m; j++) cout<<rot[i][j]<< " ";
+                cout<<endl;
+        }
+
+}
+
+
+int main(){
+        int m;
+        int arr[1000][1000];
+        cin>>m;
+        for(int i=0; i<m; i++) {
+                for(int j=0; j<m; j++) cin>>arr[i][j];
+        }
+
+        rotate(arr,m);
+        return 0;
+
+}
