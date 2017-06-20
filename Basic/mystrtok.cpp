1a61a0521333fc31263e960ceb6128568e941d97
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-20 18:46:57 +0530

    Minor Changes in strtok

--- Basic/mystrtok.cpp
+++ Basic/mystrtok.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
+
+int len(char *str){
+  int i=0;
+  while(str[i]!='\0') i++;
+  return i;
+}
+
 char *mystrtok(char *input, char delimiter){
 
         static char *pointer;
         if(input) pointer = input;
         if(!pointer) return NULL;
-        char * token = new char [20];
+        char * token = new char [len(pointer)+1];
         int i;
 
         for(i=0; pointer[i]!=delimiter&&pointer[i]!='\0'; i++) {
